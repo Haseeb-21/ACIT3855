dea34eb6d39620c5ded8f1f4c0157758f9f6d8b7
Haseeb-21 <71402178+Haseeb-21@users.noreply.github.com>
2022-04-07 15:05:49 -0700

    HAS

--- processing/app.py
+++ processing/app.py
@@ -23,6 +23,9 @@ else:
     app_conf_file = "app_conf.yml"
     log_conf_file = "log_conf.yml"
 
+with open(app_conf_file, 'a+') as f:
+    app_config = yaml.safe_load(f.read())
+
 with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
